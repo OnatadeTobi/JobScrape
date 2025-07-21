904ae61a056fa7e85beda34650c2c20e86a2c980
tobi <onatadetobi212@gmail.com>
2025-07-21 21:14:34 +0100

    Deployment Fixes 5

--- Backend/jobscrape/entrypoint.sh
+++ Backend/jobscrape/entrypoint.sh
@@ -4,10 +4,16 @@
 set -e
 
 # Run migrations
+echo "Running migrations..."
 python manage.py migrate
 
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Current DATABASE_URL: $DATABASE_URL"
+python manage.py dbshell -c "SELECT current_database();"
+
 # Start server
+echo "Starting Gunicorn..."
 exec gunicorn jobscaper_api.wsgi:application --bind 0.0.0.0:$PORT 
